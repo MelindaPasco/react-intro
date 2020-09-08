fa6e63d7cb5e7a0c9838deb48502978e58091fce
melinda <melinda.pasco@epitech.eu>
2020-09-08 19:16:00 +0200

    Updates

---
+++ precache-manifest.259331933088100b4caad0b1374af148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06899001c43f351b5e7263e4689bd016",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "deeb3f95380159aff05f",
+    "url": "/react-intro/static/css/main.f41d2635.chunk.css"
+  },
+  {
+    "revision": "9d1d08beb7689f7c3478",
+    "url": "/react-intro/static/js/2.8314528f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro/static/js/2.8314528f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deeb3f95380159aff05f",
+    "url": "/react-intro/static/js/main.5f8a4aa9.chunk.js"
+  },
+  {
+    "revision": "bbabea2acd9d0d43c754",
+    "url": "/react-intro/static/js/runtime-main.4a178d3f.js"
+  }
+]);
